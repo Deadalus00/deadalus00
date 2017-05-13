22080e59a4d141870674cd615041f281255de70b
Deadalus00 <liuyk024@protonmail.com>
2017-05-13 17:51:17 +0800

    今日作业

--- function.py
+++ function.py
@@ -19,3 +19,24 @@ def change_list(b):
 
 print change_list(b)
 print (b)
+
+#leap_year test
+def leap_year(a,b,c):
+    if a % 400 == 0:
+        if a % 100 == 0:
+            if a % 4 == 0:
+                print (True)
+            else:
+                print (False)
+        else:
+            print(True)
+    else:
+        print (False)
+        # different 
+def leap_year(a,b,c):
+     if a % 400 == 0:
+             print (True)
+     elif a % 100 == 0:
+             print(False)
+     elif a % 4 == 0:
+             print (True)
